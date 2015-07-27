f3fad9d504f725e0d4edbcab589a4d0e9f0faf22
Hoang <marknguyen431992@gmail.com>
2015-07-27 14:39:01 -0500

    firebase testing

---
+++ app/src/main/java/com/example/marknguyen/bulletyntesting/Item.java
@@ -0,0 +1,13 @@
+package com.example.marknguyen.bulletyntesting;
+
+/**
+ * Created by Hoang on 7/26/2015.
+ */
+public class Item {
+    public String author;
+    public String title;
+    public Item(String Author, String Title){
+        this.author=Author;
+        this.title=Title;
+    }
+}
--- app/src/main/java/com/example/marknguyen/bulletyntesting/MainActivity.java
+++ app/src/main/java/com/example/marknguyen/bulletyntesting/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.marknguyen.bulletyntesting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
@@ -11,9 +13,12 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Event> itemArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +27,10 @@ public class MainActivity extends AppCompatActivity {
         Firebase.setAndroidContext(this);
 
         // Get a reference to our posts
-        Firebase ref = new Firebase("https://intense-fire-8521.firebaseio.com/post/-JudY_HUxeaFvnztu6h5");
+        //Firebase ref = new Firebase("https://intense-fire-8521.firebaseio.com/post/-JudY_HUxeaFvnztu6h5");
 
         // Attach an listener to read the data at our posts reference
-
+/*
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
@@ -46,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             public void onCancelled(FirebaseError firebaseError) {
                 System.out.println("The read failed: " + firebaseError.getMessage());
             }
-        });
+        });*/
 
         //public String category;
         ///public String email;
@@ -59,15 +64,36 @@ public class MainActivity extends AppCompatActivity {
         //public String title;
 
         // Get a reference to our posts
-        //Firebase ref = new Firebase("https://docs-examples.firebaseio.com/web/saving-data/fireblog/posts");
-/*
+
+        Firebase ref = new Firebase("https://intense-fire-8521.firebaseio.com/post");
+
+        itemArrayList = new ArrayList<Event>();
+        //int index=0;
+
         ref.addChildEventListener(new ChildEventListener() {
             // Retrieve new posts as they are added to the database
             @Override
             public void onChildAdded(DataSnapshot snapshot, String previousChildKey) {
-                Map<String, Object> newPost = (Map<String, Object>) snapshot.getValue();
-                System.out.println("Author: " + newPost.get("author"));
-                System.out.println("Title: " + newPost.get("title"));
+                Map<String, String> newPost = (Map<String, String>) snapshot.getValue();
+                //System.out.println("Author: " + newPost.get("author"));
+                //System.out.println("Title: " + newPost.get("title"));
+                Event event = new Event(newPost.get("category"), newPost.get("email"),
+                        newPost.get("hashtag"), newPost.get("img"),
+                        newPost.get("info"), newPost.get("location"),
+                        newPost.get("name"), newPost.get("phone"),
+                        newPost.get("title"));
+                System.out.println(event.category);
+                System.out.println(event.email);
+                System.out.println(event.hashtag);
+                System.out.println(event.img);
+                System.out.println(event.info);
+                System.out.println(event.location);
+                System.out.println(event.name);
+                System.out.println(event.phone);
+                System.out.println(event.title);
+                Log.i("current size", String.valueOf(itemArrayList.size()));
+                itemArrayList.add(event);
+                Log.i("next size", String.valueOf(itemArrayList.size()));
             }
 
             // Get the data on a post that has changed
@@ -93,7 +119,45 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-*/
+        createView();
+    }
+
+    public void createView(){
+        if(itemArrayList.size()>0) {
+            for (int i = 0; i < itemArrayList.size(); i++) {
+                System.out.println(itemArrayList.get(i).category);
+                System.out.println(itemArrayList.get(i).email);
+                System.out.println(itemArrayList.get(i).hashtag);
+                System.out.println(itemArrayList.get(i).img);
+                System.out.println(itemArrayList.get(i).info);
+                System.out.println(itemArrayList.get(i).location);
+                System.out.println(itemArrayList.get(i).name);
+                System.out.println(itemArrayList.get(i).phone);
+                System.out.println(itemArrayList.get(i).title);
+            }
+        }
+        else{
+            System.out.println("no item added");
+        }
+    }
+
+    public void checkItem(View view){
+        if(itemArrayList.size()>0) {
+            for (int i = 0; i < itemArrayList.size(); i++) {
+                System.out.println(itemArrayList.get(i).category);
+                System.out.println(itemArrayList.get(i).email);
+                System.out.println(itemArrayList.get(i).hashtag);
+                System.out.println(itemArrayList.get(i).img);
+                System.out.println(itemArrayList.get(i).info);
+                System.out.println(itemArrayList.get(i).location);
+                System.out.println(itemArrayList.get(i).name);
+                System.out.println(itemArrayList.get(i).phone);
+                System.out.println(itemArrayList.get(i).title);
+            }
+        }
+        else{
+            System.out.println("no item added");
+        }
     }
 
     @Override
